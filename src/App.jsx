e9c0e4440ff5bd124a2ad31c95a32011c72d47e7
Beibei Zheng <beibeizhengnz@gmail.com>
2024-08-26 10:12:43 +1200

    display the main info session.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescriptions[getRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function Concepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h1>{title}</h1>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
+      <Header />
+      <main id="core-concepts">
+        <ul>
+          {CORE_CONCEPTS.map((item) => (
+            <Concepts
+              key={item.title} // Adding a unique key prop
+              image={item.image}
+              title={item.title}
+              description={item.description}
+            />
+          ))}
+        </ul>
       </main>
     </div>
   );
