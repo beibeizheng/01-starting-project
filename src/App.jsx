536114005d41182044ba5c6578249465ec8b291a
Beibei Zheng <beibeizhengnz@gmail.com>
2024-08-26 12:01:49 +1200

    add button session infomations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import reactImg from "./assets/react-core-concepts.png";
-import { CORE_CONCEPTS } from "./data.js";
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -22,33 +23,85 @@ function Header() {
   );
 }
 
+function ConceptSession() {
+  return (
+    <ul>
+      {CORE_CONCEPTS.map((item) => (
+        <Concepts
+          key={item.title} // Adding a unique key prop
+          image={item.image}
+          title={item.title}
+          description={item.description}
+        />
+      ))}
+    </ul>
+  );
+}
+
 function Concepts({ image, title, description }) {
   return (
     <li>
       <img src={image} alt={title} />
-      <h1>{title}</h1>
+      <h3>{title}</h3>
       <p>{description}</p>
     </li>
   );
 }
 
-function App() {
+function Button({ title, functionClick }) {
+  return <button onClick={functionClick}>{title}</button>;
+}
+
+function TabDetail({ index }) {
+  const example = Object.values(EXAMPLES)[index];
   return (
-    <div>
-      <Header />
-      <main id="core-concepts">
-        <ul>
-          {CORE_CONCEPTS.map((item) => (
-            <Concepts
-              key={item.title} // Adding a unique key prop
-              image={item.image}
+    <>
+      <h3>{example.title}</h3>
+      <p>{example.description}</p>
+      <pre>
+        <code>{example.code}</code>
+      </pre>
+    </>
+  );
+}
+
+function ButtonSession() {
+  const defaultText = "Please select a button";
+  const [index, setIndex] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setIndex(index);
+  };
+
+  return (
+    <>
+      <div id="examples">
+        <menu>
+          {Object.values(EXAMPLES).map((item, idx) => (
+            <Button
+              key={item.title}
               title={item.title}
-              description={item.description}
+              functionClick={() => handleButtonClick(idx)}
             />
           ))}
-        </ul>
+        </menu>
+      </div>
+      <div id="tab-content">
+        {index === null ? defaultText : <TabDetail index={index} />}
+      </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main id="core-concepts">
+        <ConceptSession />
+        <ButtonSession />
       </main>
-    </div>
+    </>
   );
 }
 
